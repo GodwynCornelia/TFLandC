4423d8b192cce6cfa23bdb93075c915f5f558b8e
Fedor <fedortk01@yandex.ru>
2026-04-20 01:31:13 +0700

    create branch

--- WinFormsApp4/WinFormsApp4/obj/Debug/net8.0-windows/WinFormsApp4.AssemblyInfo.cs
+++ WinFormsApp4/WinFormsApp4/obj/Debug/net8.0-windows/WinFormsApp4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04ff200d42cd4416df4c2b8c3ca9f830d1794844")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
